e8c43edfea5846a624ba50ea88b1867e3477ed84
alejandraPalafox <73764310+alejandraPalafox@users.noreply.github.com>
2021-01-26 19:48:10 -0600

    Update lab01.py

--- lab01/lab01.py
+++ lab01/lab01.py
@@ -22,7 +22,16 @@ def captured_output():
 
 # implement this function
 def is_perfect(n):
-    pass
+    sum = 1
+    i = 2
+    while i * i <= n: 
+        if n % i == 0: 
+            sum = sum + i + n/i
+        i += 1
+    if (sum == n and n!= 1):
+        return True
+    else:
+        return False
 
 # (3 points)
 def test1():
@@ -40,7 +49,18 @@ def test1():
 
 # implement this function
 def multiples_of_3_and_5(n):
-    pass
+    sum3 = 3
+    sum5 = 5
+    i = 3
+    y = 5
+    c = 2
+    while i*c < n:
+        sum3 = sum3 + i*c 
+        if y*c < n and (y*c%3) != 0:
+            sum5 = sum5 + y*c
+        c += 1
+    sumT = sum3+sum5
+    return sumT
 
 # (3 points)
 def test2():
@@ -53,7 +73,13 @@ def test2():
 # EXERCISE 3
 #################################################################################
 def integer_right_triangles(p):
-    pass
+  count = 0
+  for a in range(1,p):
+    for b in range(1,p):
+      c = (a**2 + b**2)**0.5
+      if (a + b + c == p):
+        count += 1
+  return(count//2)
 
 def test3():
     tc = unittest.TestCase()
@@ -67,7 +93,23 @@ def test3():
 
 # implement this function
 def gen_pattern(chars):
-    pass
+    numLetters = len(chars)*2-1
+    numDot = numLetters-1
+    total = numLetters + numDot  
+    for x in range(len(chars)-1, -1, -1):
+      lastLetter = chars[x:]
+      for y in range(x+1, len(chars)):
+        lastLetter = chars[y] + lastLetter
+      lastLetter = '.'.join(lastLetter)
+      line = lastLetter.center(total,'.')
+      print(line)
+    for z in range(1, len(chars)):
+      end = chars[z:]
+      for b in range(z+1, len(chars)):
+        end = chars[b] + end
+      end = '.'.join(end)
+      line = end.center(total,'.')
+      print(line)
 
 def test4():
     tc = unittest.TestCase()
