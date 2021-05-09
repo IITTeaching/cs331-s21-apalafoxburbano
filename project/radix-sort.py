4c0d3aa9296bdf5658145d0f9877bcaae446d5f4
alejandraPalafox <73764310+alejandraPalafox@users.noreply.github.com>
2021-05-09 12:39:27 -0500

    Update radix-sort.py

--- project/radix-sort.py
+++ project/radix-sort.py
@@ -7,4 +7,32 @@ def book_to_words(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
     return bookascii.split()
 
 def radix_a_book(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
-    pass
+    words = book_to_words()
+    maxlength = len(max(words, key = lambda x: len(x)))
+    for i in range(len(words)):
+        words[i] += bytes(maxlength - len(words[i]))
+    def radix_sort(arr, index):
+        words = book_to_words()
+    maxlength = len(max(words, key = lambda x: len(x)))
+    for i in range(len(words)):
+        words[i] += bytes(maxlength - len(words[i]))
+    def radix_sort(arr, index):
+        length = len(arr)
+        positions = [0] * 128
+        ans = [0] * length
+        for i in range(length):
+            ascii_decimal = arr[i][-1 * index]
+            positions[ascii_decimal] += 1
+        for i in range(1, len(positions)):
+            positions[i] += positions[i - 1]
+        for i in range(length-1,-1,-1):
+            ascii_decimal = arr[i][-1 * index]
+            ans[positions[ascii_decimal] - 1] = arr[i]
+            positions[ascii_decimal] -= 1
+        return ans
+    for i in range(1,maxlength + 1):
+        words = radix_sort(words,i)
+    words = [str(word, 'utf-8').rstrip('\x00') for word in words]
+    return words
+
+radix_a_book()
